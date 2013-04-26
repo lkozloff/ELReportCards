16ca28f873802890098019570d271abebea82d7b
root <root@ip-10-128-129-103.ap-southeast-1.compute.internal>
2013-04-26 09:49:50 +0700

    updating teacher selection query for teachers
    KhmerTAs are now stored in the DB, so we want to exclude their profile type.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ foreach($templates_result as $val){
 
 
 //grab staff names for list
-$query = $sdbh->prepare("SELECT staff_id, first_name, last_name from staff WHERE syear = 2012 and current_school_id=2 and profile ='teacher' and is_disable IS NULL
+$query = $sdbh->prepare("SELECT staff_id, first_name, last_name from staff WHERE syear = 2012 and current_school_id=2 and profile ='teacher' and is_disable IS NULL and profile_id=2
 		order by last_name");
 $query->execute();
 $teachers_result = $query->fetchAll(PDO::FETCH_ASSOC);
